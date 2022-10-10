459cb617ad90fc3dd10688ac95e43edcfcfcf2dc
Javier Sanchez <javier.sanchez@canonical.com>
2022-10-10 16:16:48 +0000

    Fix: unit tests

--- tests/unit/charms/lte_core_interface/v0/test_lte_core_interface_requirer.py
+++ tests/unit/charms/lte_core_interface/v0/test_lte_core_interface_requirer.py
@@ -19,12 +19,11 @@ BASE_CHARM_PATH = "tests.unit.charms.lte_core_interface.v0.dummy_requirer_charm.
 class TestCoreRequirer(unittest.TestCase):
     def setUp(self):
         self.relation_name = "lte-core"
-        self.remote_app = "lte-core-requirer"
+        self.remote_app = "lte-core-provider"
         self.harness = testing.Harness(DummyCoreRequirerCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    # TODO: AssertionError: Expected '_on_lte_core_available' to have been called.
     @patch(f"{BASE_CHARM_PATH}._on_lte_core_available")
     def test_given_valid_core_information_in_relation_data_when_relation_changed_then_core_available_event_emitted(  # noqa: E501
         self, patch_on_core_available
@@ -34,12 +33,15 @@ class TestCoreRequirer(unittest.TestCase):
         remote_app_relation_data = {"mme_ipv4_address": mme_ipv4_address}
 
         self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app, key_values=remote_app_relation_data
+            relation_id=relation_id,
+            app_or_unit=self.remote_app,
+            key_values=remote_app_relation_data,
         )
 
         patch_on_core_available.assert_called()
-        # args, _ = patch_on_core_available.call_args
-        # core_available_event = args[0]
+        args, _ = patch_on_core_available.call_args
+        lte_core_available_event = args[0]
+        self.assertEqual(lte_core_available_event.mme_ipv4_address, mme_ipv4_address)
 
     @patch(f"{BASE_CHARM_PATH}._on_lte_core_available")
     def test_given_core_information_not_in_relation_data_when_relation_changed_then_core_available_event_not_emitted(  # noqa: E501
@@ -50,16 +52,4 @@ class TestCoreRequirer(unittest.TestCase):
             relation_id=relation_id, app_or_unit=self.remote_app, key_values={}
         )
 
-        patch_on_core_available.assert_not_called()
-
-    @patch(f"{BASE_CHARM_PATH}._on_lte_core_available")
-    def test_given_invalid_core_information_in_relation_data_when_relation_changed_then_core_available_event_not_emitted(  # noqa: E501
-        self, patch_on_core_available
-    ):
-        relation_id = self.harness.add_relation(self.relation_name, remote_app=self.remote_app)
-        remote_app_relation_data = {"mme_ipv4_address": "not an ipv4 address"}
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit=self.remote_app, key_values=remote_app_relation_data
-        )
-        
         patch_on_core_available.assert_not_called()
